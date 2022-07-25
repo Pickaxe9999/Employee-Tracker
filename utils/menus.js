ff0c4e7c26ad135ee9dddac0af02deb52412832e
Jacob H <jacob.hannah164@gmail.com>
2022-07-25 18:21:41 -0400

    created a function for the option 'add ne employee' however having a hard time referencing back to the original SQL table to get manager id

--- utils/menus.js
+++ utils/menus.js
@@ -3,7 +3,7 @@ const db = require('../db/connection');
 const cTable = require('console.table');
 
 
-
+//main menu of the application
 const mainMenu =  async function(){
     const { option } = await inquirer.prompt({
         type: 'list',
@@ -39,7 +39,83 @@ const mainMenu =  async function(){
     }
 }
 
-//employee queries
+//user menu for adding a new employee
+const promptForEmployee = async function(){
+    //query the current information for all departments
+    const departmentSQL = `SELECT name, id FROM department`;
+    const departments = await db.promise().query(departmentSQL).then(rows => {
+        rows = rows[0];
+        return rows;
+    });
+
+    //query the current information for all managers
+    const managerSQL = `SELECT first_name, last_name, id FROM employees WHERE manager_id IS NULL`;
+    const managers = await db.promise().query(managerSQL).then(rows => {
+        rows = rows[0];
+        return rows;
+    });
+
+
+    //join the manager first and last names together into one string per index
+    let displayManagers = [];
+    for (let i = 0; i < managers.length; i++) {
+        displayManagers.push(managers[i].first_name + ' ' + managers[i].last_name);
+    }
+    displayManagers.push('No Manager');
+
+
+    console.log(managers);
+    console.log(displayManagers);
+
+
+
+    //Ask for the new employees information
+    let employee = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "What is the employee's first name?",
+            validate: name => {
+                if (name){
+                    return true;
+                } else {
+                    console.log("Please enter the employee's first name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "What is the employee's last name?",
+            validate: name => {
+                if (name){
+                    return true;
+                } else {
+                    console.log("Please enter the employee's last name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'rawlist',
+            message: 'What department is this employee in?',
+            name: 'department_id',
+            choices: departments
+        },
+        {
+            type: 'list',
+            message: 'What manager does this employee have?',
+            name: 'manager_id',
+            choices: managers
+        }
+    ])
+
+    console.log(employee);
+}
+
+
+//employee queries & functions
 const viewAllEmployees = function(){
     const sql = `SELECT * FROM employees`;
     db.promise().query(sql)
@@ -51,8 +127,8 @@ const viewAllEmployees = function(){
 }
 
 const addEmployee = function(){
-    console.log('add employee menu');
-    return mainMenu();
+    sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+    promptForEmployee();
 }
 
 const updateEmployeeRole = function(){
